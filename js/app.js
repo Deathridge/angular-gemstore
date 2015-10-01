60eb0bdc871c832d8c3728fab36047a72d4b1291
Deathridge <danielrbetteridge@gmail.com>
2015-10-01 17:40:24 +0800

    work on forms, models, more controllers etc

--- js/app.js
+++ js/app.js
@@ -2,14 +2,23 @@
 	var app = angular.module('gemStore', []);
 
 app.controller('StoreController', function(){
-	this.products = gems;
-});
-
-var gems = [
+	this.products = [
 {
 	name: 'Dodecahedron',
 	price: 2.95,
 	description: '...',
+	images: [{
+		full: 'images/gem-01.gif',		
+	}	
+	],
+
+	reviews: [
+	{
+		stars: 5,
+		body: "I love this product",
+		author: "joe@thomas.com"
+	}
+	],
 	canPurchase: true,
 	
 },
@@ -17,9 +26,44 @@ var gems = [
 	name: "Pentagonal Gem",
 	price: 5.95,
 	description: "...",
+	images: [{
+		full: 'images/gem-02.gif',		
+	}	
+	],
+	reviews: [
+	{
+		stars: 5,
+		body: "I love this product",
+		author: "joe@thomas.com"
+	}
+	],
 	canPurchase: true,
 	
 }
 ];
+});
+
+app.controller("PanelController", function(){
+	this.tab = 1;
+
+	this.selectTab = function(setTab){
+		this.tab = setTab;
+
+	};
+
+	this.isSelected = function(checkTab){
+		return this.tab === checkTab;
+	};
+});
+
+app.controller("ReviewController", function(){
+	this.review={};
+
+	this.addReview = function(product){
+		product.reviews.push(this.review);
+		this.review={};
+	};
+
+});
 
 })();
